991857ba6e0b8ed44f40c3fbf05e29ba57c898f1
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2014-12-09 01:54:05 -0500

    set up flask application, added font, added html and css files

--- reddit.py
+++ reddit.py
@@ -1,9 +1,25 @@
 from flask import Flask
 from flask import request
+from flask import render_template
+from newspaper import Article
 import praw
 
 
 app = Flask(__name__)
+
+
+class RedditPost():
+    def __init__(self, post):
+        self.reddit_title = post.title
+
+        # use newspaper module to download article information
+        article = Article(url=post.url)
+        article.download()
+        article.parse()
+
+        self.article_title = article.title
+        self.image = article.top_image
+
 
 
 def get_useragent():
@@ -12,8 +28,15 @@ def get_useragent():
 
 @app.route('/')
 def homepage():
-    news = get_useragent().get_subreddit('worldnews').get_hot(limit=5)
-    return str([str(x) for x in news])
+    world_news_posts = get_useragent().get_subreddit('worldnews').get_hot(limit=2)
+
+    posts = []
+    for p in world_news_posts:
+        post = RedditPost(p)
+        posts.append(post)
+
+
+    return render_template('index.html', post_list=posts)
 
 
 if __name__ == '__main__':
