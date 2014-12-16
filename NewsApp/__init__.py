131811d831ffea21e6a872b3dc89b725bf686fbc
jlaskin <jlaskin@seas.upenn.edu>
2014-12-15 21:00:12 -0500

    Update __init__.py

--- NewsApp/__init__.py
+++ NewsApp/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.secret_key = 'development key'
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/databasename'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:cheese1223@localhost/development'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/development'
 
 from models import db
 db.init_app(app)
