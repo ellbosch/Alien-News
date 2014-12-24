9eef8c259200a3a1a0cc1ddbcb2e53dc75668c3e
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2014-12-24 12:01:02 -1000

    new branch removing signup functionality

--- reddit.py
+++ reddit.py
@@ -1,7 +1,5 @@
-from flask import Flask
-from flask import request
-from flask import render_template
 from newspaper import Article
+from flask import Flask, render_template, request, jsonify
 import praw
 
 
@@ -11,32 +9,60 @@ app = Flask(__name__)
 class RedditPost():
     def __init__(self, post):
         self.reddit_title = post.title
+        self.url = post.url
 
         # use newspaper module to download article information
-        article = Article(url=post.url)
+        # article = Article(url=post.url)
+        # article.download()
+        # article.parse()
+
+        # self.article_title = article.title
+        # self.image = article.top_image
+
+
+class ArticlePost():
+    def __init__(self, url):
+        article = Article(url=url)
         article.download()
         article.parse()
 
-        self.article_title = article.title
+        self.title = article.title
         self.image = article.top_image
-
+        self.text = article.text
 
 
 def get_useragent():
     return praw.Reddit(user_agent='Alien News/1.0 by ellbosch')
 
 
-@app.route('/')
-def homepage():
-    world_news_posts = get_useragent().get_subreddit('worldnews').get_hot(limit=2)
-
+def get_reddit_posts(subreddit, n):
+    subreddit_posts = get_useragent().get_subreddit(subreddit).get_hot(limit=n)
     posts = []
-    for p in world_news_posts:
+    for p in subreddit_posts:
         post = RedditPost(p)
         posts.append(post)
 
+    return posts
 
-    return render_template('index.html', post_list=posts)
+
+@app.route('/')
+def homepage():
+    sub = 'worldnews'
+    posts = get_reddit_posts(sub, 10)
+
+    article = ArticlePost(posts[0].url)
+
+    return render_template('index.html', post_list=posts, article=article) 
+
+
+@app.route('/article/')
+def show_article():
+    url = str(request.args['url'])
+
+    article = ArticlePost(url)
+
+    return jsonify(result = {"title": article.title,
+                             "text": article.text})
 
 
 if __name__ == '__main__':
---
+++ static/script.js
@@ -0,0 +1,60 @@
+$(function() {
+	var NAMESPACE = {
+		article_width: $("#article_div").width()
+	}
+
+	function init() {
+		resize_posts_div();
+	}
+
+	function resize_posts_div() {
+		width = $(window).width() - NAMESPACE.article_width;
+		$("#posts_list_div").css('width', width);
+	}
+
+	// window resize events
+	$(window).smartresize(function() {
+		resize_posts_div();
+	});
+
+	// toggles sidebar
+	$("#sidebar_button").click(function() {
+		$("#sidebar").show("slide");
+	});
+	$("#close_button").click(function() {
+		$("#sidebar").hide("slide");
+	});
+
+	// function call that runs on page load
+	init();
+
+});
+
+
+(function($,sr){
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+  	var timeout;
+
+  	return function debounced () {
+  		var obj = this, args = arguments;
+  		function delayed () {
+  			if (!execAsap)
+  				func.apply(obj, args);
+  			timeout = null;
+  		};
+
+  		if (timeout)
+  			clearTimeout(timeout);
+  		else if (execAsap)
+  			func.apply(obj, args);
+
+  		timeout = setTimeout(delayed, threshold || 100);
+  	};
+  }
+  // smartresize 
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
